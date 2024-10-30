3ee9907eaa7fa08abc2da0c2b40ae9cffe184567
rcanfield <Rcanfield86@gmail.com>
2024-10-30 10:48:53 -0400

    Add motion to empty cart

--- apps/host/src/app/features/Cart/components/EmptyCart.tsx
+++ apps/host/src/app/features/Cart/components/EmptyCart.tsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-router-dom';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { motion } from 'framer-motion';
 
 const EmptyCart: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-gray-100 dark:bg-gray-900 p-8">
+    <motion.div
+      className="flex flex-col items-center justify-center min-h-screen w-full bg-gray-100 dark:bg-gray-900 p-8"
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.9 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="max-w-2xl w-full">
         <ShoppingCartIcon className="w-16 h-16 text-gray-400 mb-4 mx-auto" />
         <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200 mb-2 text-center">
@@ -22,7 +29,7 @@ const EmptyCart: React.FC = () => {
           </Link>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
